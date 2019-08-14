2658d6469437b98f007c493696944d237847adea
abhishekChouhan <chouhanabhi5@gmail.com>
2019-08-14 18:53:31 +0530

    major fix

--- main.py
+++ main.py
@@ -6,119 +6,117 @@ app = Flask(__name__)
 
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-newx, newy, newz, newtext = [], [], [], []
-new_fullx, new_fully, new_fullz, new_fulltext = [], [], [], []
+JSON = {}
+
+def set_data():
+    global JSON
+    with open('static/default_figure.js', encoding="utf8") as dataFile:
+        data = dataFile.read()
+        obj = data[data.find('{') : data.rfind('}')+1]
+        jsonObj = json.loads(obj)
+        dataFile.close()
+
+    JSON = jsonObj
+
 
 def show_searched_movie_plot(movie_title):
+    near = 1
+    far = 40
+    newx, newy, newz, newtext = [], [], [], []
+    new_fullx, new_fully, new_fullz, new_fulltext = [], [], [], []
+    local_json = JSON
 
-	with open('static/default_figure.js', encoding="utf8") as dataFile:
-	    data = dataFile.read()
-	    obj = data[data.find('{') : data.rfind('}')+1]
-	    jsonObj = json.loads(obj)
-	    dataFile.close()
+    search = movie_title
+    if search=='':
+        # with open('static/figure.js', 'w') as f:
+        #     full = 'var figure = ' + str(local_json)
+        #     f.write(full)
+        #     f.close()
+        #     is_success = 0
 
-	near = 1
-	far = 40
+        return local_json['data']
 
-	search = movie_title
-	if search=='':
-		with open('static/figure.js', 'w') as f:
-			full = 'var figure = ' + str(jsonObj)
-			f.write(full)
-			f.close()
-			is_success = 0
-
-		return is_success
-
-	data_ob_0 = jsonObj['data'][0]
-	data_ob_1 = jsonObj['data'][1]
-	listx = data_ob_0['x']
-	listy = data_ob_0['y']
-	listz = data_ob_0['z']
-	listtext = data_ob_0['text']
-
-	pos = listtext.index(search)
-	val_x = listx[pos]
-	val_y = listy[pos]
-	val_z = listz[pos]
-	print('postion of ' + search + ':' + str(val_x) + " " + str(val_y) + " " + str(val_z) )
-	newx.clear()
-	newy.clear()
-	newz.clear()
-	newtext.clear()
-	new_fullx.clear()
-	new_fully.clear()
-	new_fullz.clear()
-	new_fulltext.clear()
     
-	for i in range(len(listx)):
-		if listx[i]>=(val_x-near) and listx[i]<=(val_x+near) and listy[i]>=(val_y-near) and listy[i]<=(val_y+near) and listz[i]>=(val_z-near) and listz[i]<=(val_z+near):
-			#print(str(listx[i]) + " " + str(listy[i]) + " " + str(listz[i]) )
-			#print(listtext[i])
-			newx.append(listx[i])
-			newy.append(listy[i])
-			newz.append(listz[i])
-			#pos_list.append(i)
-			newtext.append(listtext[i])
-		elif listx[i]>=(val_x-far) and listx[i]<=(val_x+far) and listy[i]>=(val_y-far) and listy[i]<=(val_y+far) and listz[i]>=(val_z-far) and listz[i]<=(val_z+far):
-			new_fullx.append(listx[i])
-			new_fully.append(listy[i])
-			new_fullz.append(listz[i])
-			#pos_list.append(i)
-			new_fulltext.append(listtext[i])
+    data_0 = local_json['data'][0]
+    data_1 = local_json['data'][1]
 
+    FULLX, FULLY, FULLZ, FULLTEXT = data_0['x'], data_0['y'], data_0['z'], data_0['text']
+    # EMPYTX, EMPYTY, EMPYTZ, EMPYTTEXT, = EMPTY_DATA['x'], EMPTY_DATA['y'], EMPTY_DATA['z'], EMPTY_DATA['text']
 
-	data_ob_1['x'] = newx
-	data_ob_1['y'] = newy
-	data_ob_1['z'] = newz
-	data_ob_1['text'] = newtext
-	jsonObj['data'][1] = data_ob_1
+    pos = FULLTEXT.index(search)
+    val_x, val_y, val_z = FULLX[pos], FULLY[pos], FULLZ[pos]
 
-	data_ob_0['x'] = new_fullx
-	data_ob_0['y'] = new_fully
-	data_ob_0['z'] = new_fullz
-	data_ob_0['text'] = new_fulltext
-	jsonObj['data'][0] = data_ob_0
-	is_success = -1
-	jsonObj = json.dumps(jsonObj, indent=4)
+    print('postion of ' + search + ':' + str(val_x) + " " + str(val_y) + " " + str(val_z) )
+    
+    for i in range(len(FULLX)):
+        if FULLX[i]>=(val_x-near) and FULLX[i]<=(val_x+near) and FULLY[i]>=(val_y-near) and FULLY[i]<=(val_y+near) and FULLZ[i]>=(val_z-near) and FULLZ[i]<=(val_z+near):
+            #print(str(FULLX[i]) + " " + str(FULLY[i]) + " " + str(FULLZ[i]) )
+            #print(FULLTEXT[i])
+            newx.append(FULLX[i])
+            newy.append(FULLY[i])
+            newz.append(FULLZ[i])
+            #pos_list.append(i)
+            newtext.append(FULLTEXT[i])
+        elif FULLX[i]>=(val_x-far) and FULLX[i]<=(val_x+far) and FULLY[i]>=(val_y-far) and FULLY[i]<=(val_y+far) and FULLZ[i]>=(val_z-far) and FULLZ[i]<=(val_z+far):
+            new_fullx.append(FULLX[i])
+            new_fully.append(FULLY[i])
+            new_fullz.append(FULLZ[i])
+            #pos_list.append(i)
+            new_fulltext.append(FULLTEXT[i])
 
-	#print(jsonObj)
+    data_0['x'] = new_fullx
+    data_0['y'] = new_fully
+    data_0['z'] = new_fullz
+    data_0['text'] = new_fulltext
+    local_json['data'][0] = data_0
 
-	with open('static/figure.js', 'w') as f:
-		full = 'var figure = ' + str(jsonObj)
-		f.write(full)
-		f.close()
-		is_success = 0
+    data_1['x'] = newx
+    data_1['y'] = newy
+    data_1['z'] = newz
+    data_1['text'] = newtext
+    local_json['data'][1] = data_1
 
-	return is_success
+    
+    # is_success = -1
+    # local_json = json.dumps(local_json, indent=4)
+
+    #print(local_json)
+
+    # with open('static/figure.js', 'w') as f:
+    #     full = 'var figure = ' + str(local_json)
+    #     f.write(full)
+    #     f.close()
+    #     is_success = 0
+
+    # return is_success
+    return local_json['data']
+
 
 @app.route('/')
 def index():
-	return render_template('index.html')
+    return render_template('index.html')
 
-@app.route('/search', methods=['POST','GET'])
+@app.route('/search', methods=['POST'])
 def search():
-	movie_title = request.args.get("movie_name")
-	print('nnnnnn'+movie_title)
-	# if request.method == 'POST':
-	# 	movie_title = request.form['myMovie']
-	# 	is_success = show_searched_movie_plot(movie_title)
-	# 	print(is_success)
-	# 	print(request.form['myMovie'])
-	x1, y1, z1, text1 = [], [], [], []
-	x2, y2, z2, text2 = [], [], [], []
-	x1 = newx
-	y1 = newx
-	z1 = newz
-	text1 = newtext
-	x2 = new_fullx
-	y2 = new_fully
-	z2 = new_fullz
-	print('len of sugesstions: ' + str(len(x1)))
-	print('len of total is: ' + str(len(x2) + len(x1)))
-	text2 = new_fulltext
-	return jsonify(movie_title=movie_title, x1=x1, y1=y1, z1=z1, text1=text1, x2=x2, y2=y2, z2=z2, text2=text2)
+    movie_title = request.args.get("movie_name")
+    # movie_title = 'Predator 2'
+    # print('nnnnnn'+movie_title)
+    # if request.method == 'POST':
+    #   movie_title = request.form['myMovie']
+    #   is_success = show_searched_movie_plot(movie_title)
+    #   print(is_success)
+    #   print(request.form['myMovie'])
+    big_data, small_data = show_searched_movie_plot(movie_title)
+
+    x1, y1, z1, text1 = small_data['x'], small_data['y'], small_data['z'], small_data['text']
+    x2, y2, z2, text2 = big_data['x'], big_data['y'], big_data['z'], big_data['text']
+
+    print('len of sugesstions: ' + str(len(x1)))
+    print('len of total is: ' + str(len(x2) + len(x1)))
+
+    return jsonify(x1=x1, y1=y1, z1=z1, text1=text1, x2=x2, y2=y2, z2=z2, text2=text2)
 
 
 if __name__ == '__main__':
-   app.run(debug = True)
+    set_data()
+    app.run(debug = True)
